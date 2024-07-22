63facb1e64ac68664d92616f7ab44830758b257c
Miroslav Tsarev <mtzarev@gmail.com>
2024-07-22 22:56:15 +0300

    Update 1.NewEvenPositionElement.js

--- 1.NewEvenPositionElement.js
+++ 1.NewEvenPositionElement.js
@@ -1,11 +1,10 @@
 function evenPositionElement(input) {
 let newArr = [];
-for (let i=0; i<input.length; i++){
-    
+for (let i=0; i<input.length; i++){  
 if (i%2==0){
     newArr.push(input[i]);
-}
-}
+        }
+    }
 console.log(newArr.join(` `));
 
 }
